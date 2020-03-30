19776acddb1c5cb257778e313c8e93fcfc71f67f
mmontecinoa <mmontecinoa@bitbucket.org>
2020-03-30 20:20:40 -0300

    Primer commit

---
+++ MP.StockHouse/MP.StockHouse.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MP.StockHouse.Web
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/admin-lte").Include(
+                      "~/admin-lte/css/alt/AdminLTE-*",
+                      "~/admin-lte/css/Admin*",
+                      "~/admin-lte/css/skins/_all-skins.min.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/admin-lte").Include(
+                      "~/admin-lte/js/Admin*"/*,
+                      "~/admin-lte/js/pages/dashboard*"*/));
+
+            bundles.Add(new StyleBundle("~/Content/font").Include(
+                      "~/Content/font-awesome.min.css"));
+
+        }
+    }
+}
---
+++ MP.StockHouse/MP.StockHouse.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MP.StockHouse.Web.MvcApplication" Language="C#" %>
